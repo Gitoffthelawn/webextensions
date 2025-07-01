fd3bf34c717ed023feb3ba5ad9ea58ace0de858c
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-07-01 19:50:46 +0200

    chore: add tabs-media-controller

---
+++ tabs-media-controller/content.js
@@ -0,0 +1,233 @@
+/*global browser */
+
+function getMediaElementBy(id) {
+  return document.querySelector('[tmcuuid="' + id + '"]');
+}
+
+function getThumbnail(video) {
+  let canvas = document.createElement("canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0, 300, 200);
+  return canvas.toDataURL("image/jpeg", 0.3);
+}
+
+function handleQuery(id) {
+  const el = getMediaElementBy(id);
+
+  return {
+    poster:
+      el.tagName.toLowerCase() === "video" ? getThumbnail(el) : "audio.png",
+    duration: el.duration,
+    currentTime: el.currentTime,
+    playing: !el.paused,
+    volume: el.volume,
+    muted: el.muted,
+    playbackRate: el.playbackRate,
+    id,
+  };
+}
+
+// get all media elements and their states
+function handleQueryAll() {
+  const ret = [];
+  const els = document.querySelectorAll("video,audio");
+  for (const el of els) {
+    let tmcuuid = el.getAttribute("tmcuuid");
+    if (tmcuuid === null) {
+      tmcuuid = crypto.randomUUID();
+      el.setAttribute("tmcuuid", tmcuuid);
+    }
+
+    if (
+      !isNaN(el.duration) // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/duration
+    ) {
+      ret.push({
+        poster: el.tagName.toLowerCase() === "video" ? getThumbnail(el) : "",
+        type: el.tagName.toLowerCase(),
+        duration: el.duration,
+        currentTime: el.currentTime,
+        playing: !el.paused,
+        volume: el.volume,
+        muted: el.muted,
+        playbackRate: el.playbackRate,
+        id: tmcuuid,
+      });
+    }
+  }
+
+  return ret;
+}
+
+function handlePreview(id) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    return getThumbnail(el);
+  }
+  return "";
+}
+
+function handlePause(ids) {
+  for (const id of ids) {
+    let el = getMediaElementBy(id);
+    if (el) {
+      el.pause();
+    }
+  }
+  return "play";
+}
+
+function handlePauseAll() {
+  for (const el of document.querySelectorAll("video,audio")) {
+    el.pause();
+  }
+}
+
+function handleMuteAll() {
+  for (const el of document.querySelectorAll("video,audio")) {
+    el.muted = true;
+  }
+}
+
+function handlePlay(ids) {
+  for (const id of ids) {
+    let el = getMediaElementBy(id);
+    if (el) {
+      el.play();
+    }
+  }
+}
+
+function handleMute(ids) {
+  for (const id of ids) {
+    let el = getMediaElementBy(id);
+    if (el) {
+      el.muted = true;
+    }
+  }
+}
+
+function handleUnMute(ids) {
+  for (const id of ids) {
+    let el = getMediaElementBy(id);
+    if (el) {
+      el.muted = false;
+    }
+  }
+}
+
+function handlePIP(ids) {
+  for (const id of ids) {
+    let el = getMediaElementBy(id);
+    if (el) {
+      // not supported ref. https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/requestPictureInPicture
+      // and since bug https://bugzilla.mozilla.org/show_bug.cgi?id=1463402
+      // is closed with WONTFIX, this feature wont be available any time soon it seems
+      el.requestPictureInPicture();
+    }
+  }
+}
+
+async function handleFullscreen(id) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    try {
+      el.requestFullscreen(); // not doable since action is not recognized as a short running user action
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+async function handlePlaybackRate(id, playbackRate) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    try {
+      el.playbackRate = playbackRate;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+async function handleVolume(id, volume) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    try {
+      el.volume = volume;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+async function handleCurrentTime(id, currentTime) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    try {
+      el.currentTime = currentTime;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+async function handleFocus(id) {
+  let el = getMediaElementBy(id);
+  if (el) {
+    try {
+      el.scrollIntoView();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+browser.runtime.onMessage.addListener((request) => {
+  //console.debug("onMessage", JSON.stringify(request, null, 4));
+  switch (request.cmd) {
+    case "query":
+      return Promise.resolve(handleQuery(request.id));
+    case "queryAll":
+      return Promise.resolve(handleQueryAll());
+    case "play":
+      return Promise.resolve(handlePlay(request.ids));
+    case "pause":
+      return Promise.resolve(handlePause(request.ids));
+    case "pauseAll":
+      return Promise.resolve(handlePauseAll());
+    case "mute":
+      return Promise.resolve(handleMute(request.ids));
+    case "muteAll":
+      return Promise.resolve(handleMuteAll());
+    case "unmute":
+      return Promise.resolve(handleUnMute(request.ids));
+    case "pip":
+      return Promise.resolve(handlePIP(request.ids));
+    case "fullscreen":
+      return Promise.resolve(handleFullscreen(request.ids));
+    case "volume":
+      return Promise.resolve(handleVolume(request.id, request.volume));
+    case "playbackRate":
+      return Promise.resolve(
+        handlePlaybackRate(request.id, request.playbackRate),
+      );
+    case "currentTime":
+      return Promise.resolve(
+        handleCurrentTime(request.id, request.currentTime),
+      );
+    case "focus":
+      return Promise.resolve(handleFocus(request.id));
+    case "preview":
+      return Promise.resolve(handlePreview(request.id));
+    default:
+      console.error("unknown request", request);
+      break;
+  }
+});
+
+setTimeout(() => {
+  //console.debug("injecting media.js into document head ");
+  var s = document.createElement("script");
+  s.src = browser.runtime.getURL("attach.js");
+  s.onload = () => s.remove();
+  document.head.appendChild(s);
+}, 3000);
---
+++ tabs-media-controller/popup.js
@@ -0,0 +1,436 @@
+/* global browser */
+
+async function getFromStorage(type, id, fallback) {
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
+}
+
+const tablist = document.getElementById("tabs");
+
+function addDataListToRange(numberArray, rangeEl, attachEl) {
+  const datalistid = "rangeSteps" + Date.now();
+
+  rangeEl.setAttribute("list", datalistid);
+  let datalistEl = document.createElement("datalist");
+  datalistEl.setAttribute("id", datalistid);
+
+  numberArray.forEach((val) => {
+    let opt = document.createElement("option");
+    opt.innerText = val;
+    datalistEl.appendChild(opt);
+  });
+
+  attachEl.appendChild(datalistEl);
+}
+
+async function queryTabs() {
+  let first = true;
+
+  const tabs = await browser.tabs.query({
+    url: ["<all_urls>"],
+    discarded: false,
+    status: "complete",
+  });
+
+  // lets move the audible tabs to the front/top
+  tabs.sort((a, b) => {
+    if (a.audible && !b.audible) {
+      return -1;
+    }
+    if (b.audible && !a.audible) {
+      return 1;
+    }
+    return 0;
+  });
+
+  for (const tab of tabs) {
+    try {
+      const res = await browser.tabs.sendMessage(tab.id, { cmd: "queryAll" });
+      //console.debug(JSON.stringify(res, null, 4));
+
+      res.sort((a, b) => {
+        if (a.playing && !b.playing) {
+          return -1;
+        }
+        if (b.playing && !a.playing) {
+          return 1;
+        }
+        if (a.playing && b.playing) {
+          if (!a.muted && b.muted) {
+            return -1;
+          }
+          if (a.muted && !b.muted) {
+            return 1;
+          }
+          // both are muted or unmuted => equal
+        }
+        return 0;
+      });
+
+      if (res.length > 0) {
+        if (first) {
+          tablist.textContent = "";
+          first = false;
+        }
+
+        const url = new URL(tab.url);
+        const favIconUrl = tab.favIconUrl || "";
+
+        let tabdiv = document.createElement("div");
+        tabdiv.classList.add("tabDiv");
+        /*let seperator = document.createElement("hr");
+        tabdiv.appendChild(seperator);*/
+
+        tablist.appendChild(tabdiv);
+
+        let tabFavImg = document.createElement("img");
+        tabFavImg.classList.add("tabFavImg");
+        tabFavImg.src = favIconUrl;
+        let tablink = document.createElement("button");
+
+        tablink.textContent =
+          "#" +
+          tab.index +
+          " " +
+          url.hostname.replace(/^www\./, "") +
+          " - " +
+          tab.title;
+        tablink.classList.add("tabFocusBtn");
+        tabdiv.appendChild(tablink);
+        tablink.setAttribute("title", "focus tab");
+        tablink.onclick = () => {
+          browser.tabs.highlight({ windowId: tab.windowId, tabs: [tab.index] });
+        };
+        tablink.appendChild(tabFavImg);
+
+        let mutetabbtn = document.createElement("button");
+        mutetabbtn.textContent = "mute(tab)";
+        mutetabbtn.classList.add("tabMuteBtn");
+        mutetabbtn.setAttribute("title", "mute tab");
+        tabdiv.appendChild(mutetabbtn);
+        mutetabbtn.onclick = async () => {
+          browser.tabs.sendMessage(tab.id, {
+            cmd: "muteAll",
+          });
+        };
+
+        let pausetabbtn = document.createElement("button");
+        pausetabbtn.textContent = "pause(tab)";
+        pausetabbtn.classList.add("tabPauseBtn");
+        pausetabbtn.setAttribute("title", "pause tab");
+        tabdiv.appendChild(pausetabbtn);
+        pausetabbtn.onclick = () => {
+          browser.tabs.sendMessage(tab.id, { cmd: "pauseAll" });
+        };
+
+        let pauseOriginbtn = document.createElement("button");
+        pauseOriginbtn.textContent = "pause(site)";
+        pauseOriginbtn.classList.add("sitePauseBtn");
+        pauseOriginbtn.setAttribute("title", "pause site");
+        tabdiv.appendChild(pauseOriginbtn);
+        pauseOriginbtn.onclick = () => {
+          for (const tt of tabs) {
+            if (tt.url.startsWith(url.origin)) {
+              browser.tabs.sendMessage(tt.id, { cmd: "pauseAll" });
+            }
+          }
+        };
+
+        let muteOriginbtn = document.createElement("button");
+        muteOriginbtn.textContent = "mute(site)";
+        muteOriginbtn.classList.add("siteMuteBtn");
+        muteOriginbtn.setAttribute("title", "mute site");
+        tabdiv.appendChild(muteOriginbtn);
+        muteOriginbtn.onclick = async () => {
+          for (const tt of tabs) {
+            if (tt.url.startsWith(url.origin)) {
+              browser.tabs.sendMessage(tt.id, {
+                cmd: "muteAll",
+              });
+            }
+          }
+        };
+
+        for (const e of res) {
+          // media element
+          let elementrow = document.createElement("div");
+          elementrow.setAttribute("eid", e.id);
+          elementrow.setAttribute("tid", tab.id);
+          elementrow.classList.add("elementDiv");
+          tabdiv.appendChild(elementrow);
+
+          // poster
+          let previewImg = document.createElement("img");
+          previewImg.src = e.poster || "audio.png";
+          previewImg.classList.add("previewImg");
+          elementrow.appendChild(previewImg);
+
+          let controls = document.createElement("div");
+          controls.classList.add("elementControlsDiv");
+          elementrow.appendChild(controls);
+
+          // focus
+          let focusbtn = document.createElement("button");
+          focusbtn.textContent = "focus";
+          focusbtn.classList.add("elementFocusBtn");
+          focusbtn.setAttribute("title", "focus element");
+          controls.appendChild(focusbtn);
+          focusbtn.onclick = async (evt) => {
+            await browser.windows.update(tab.windowId, { focused: true });
+            await browser.tabs.highlight({
+              windowId: tab.windowId,
+              tabs: [tab.index],
+            });
+            await browser.tabs.sendMessage(tab.id, {
+              cmd: evt.target.textContent,
+              id: e.id,
+            });
+          };
+
+          // playbackRate
+          let playbackRatebtn = document.createElement("input");
+          playbackRatebtn.setAttribute("type", "range");
+          playbackRatebtn.setAttribute("min", "25");
+          playbackRatebtn.setAttribute("max", "175");
+          playbackRatebtn.setAttribute("step", "1");
+          playbackRatebtn.setAttribute("value", e.playbackRate * 100);
+          playbackRatebtn.classList.add("elementPlaybackRateBtn");
+          playbackRatebtn.setAttribute(
+            "title",
+            "Playback Rate: " + e.playbackRate,
+          );
+
+          addDataListToRange(
+            [25, 50, 75, 100, 125, 150, 175],
+            playbackRatebtn,
+            controls,
+          );
+          controls.appendChild(playbackRatebtn);
+          playbackRatebtn.addEventListener("input", (evt) => {
+            browser.tabs.sendMessage(tab.id, {
+              cmd: "playbackRate",
+              id: e.id,
+              playbackRate: evt.target.value / 100,
+            });
+
+            evt.target.setAttribute(
+              "title",
+              "Playback Rate: " + evt.target.value / 100,
+            );
+          });
+
+          // mute
+          let mutebtn = document.createElement("button");
+          mutebtn.textContent = e.muted ? "unmute" : "mute";
+          mutebtn.classList.add("elementMuteBtn");
+          mutebtn.setAttribute("title", "un/mute element");
+          controls.appendChild(mutebtn);
+          mutebtn.onclick = (evt) => {
+            browser.tabs.sendMessage(tab.id, {
+              cmd: evt.target.textContent,
+              ids: [e.id],
+            });
+          };
+
+          // volume
+          let volumebtn = document.createElement("input");
+          volumebtn.setAttribute("type", "range");
+          volumebtn.setAttribute("min", "0");
+          volumebtn.setAttribute("max", "100");
+          volumebtn.setAttribute("step", "1");
+          volumebtn.setAttribute("value", e.volume * 100);
+          volumebtn.classList.add("elementVolumeBtn");
+          volumebtn.setAttribute("title", "change volume");
+          addDataListToRange([0, 25, 50, 75, 100], volumebtn, controls);
+          controls.appendChild(volumebtn);
+
+          volumebtn.addEventListener("input", (evt) => {
+            browser.tabs.sendMessage(tab.id, {
+              cmd: "volume",
+              id: e.id,
+              volume: evt.target.value / 100,
+            });
+          });
+
+          // play / pause
+          let playpausebtn = document.createElement("button");
+          playpausebtn.textContent = e.playing ? "pause" : "play";
+          playpausebtn.classList.add("elementPlayPauseBtn");
+          playpausebtn.setAttribute("title", "play/pause element");
+          controls.appendChild(playpausebtn);
+          playpausebtn.onclick = async (evt) => {
+            browser.tabs.sendMessage(tab.id, {
+              cmd: evt.target.textContent,
+              ids: [e.id],
+            });
+          };
+
+          // currentTime
+          let currentTimebtn = document.createElement("input");
+          currentTimebtn.setAttribute("type", "range");
+          currentTimebtn.setAttribute("min", "0");
+          currentTimebtn.setAttribute("max", "" + parseInt(e.duration));
+          if (e.duration === -1) {
+            currentTimebtn.setAttribute("disabled", "disabled");
+          }
+          currentTimebtn.setAttribute("value", e.currentTime);
+
+          currentTimebtn.classList.add("elementTimeBtn");
+          currentTimebtn.setAttribute("title", "change playback position");
+
+          addDataListToRange(
+            [
+              0,
+              parseInt(e.duration) / 4,
+              parseInt(e.duration) / 2,
+              (parseInt(e.duration) / 4) * 3,
+              parseInt(e.duration),
+            ],
+            currentTimebtn,
+            controls,
+          );
+
+          controls.appendChild(currentTimebtn);
+          currentTimebtn.addEventListener("input", (evt) => {
+            browser.tabs.sendMessage(tab.id, {
+              cmd: "currentTime",
+              id: e.id,
+              currentTime: parseInt(evt.target.value),
+            });
+          });
+        }
+      }
+    } catch (e) {
+      console.error("tab ", tab.index, e);
+    }
+  }
+  tablist.focus();
+}
+
+(async () => {
+  const styles = await getFromStorage(
+    "string",
+    "styles",
+    `
+/* The World is your canvas, have fun */
+
+body {
+  padding:0;
+  margin:0;
+  background-color:silver;
+}
+#tabs {
+  padding:0;
+  margin:0;
+}
+.tabDiv {
+  padding:10px;
+}
+.tabFocusBtn {
+  width: 100%;
+  word-break: break-all;
+}
+.tabMuteBtn {
+  width: 25%;
+}
+.tabPauseBtn {
+  width: 25%;
+}
+.sitePauseBtn {
+  width: 25%;
+}
+.siteMuteBtn {
+  width: 25%;
+}
+.elementDiv {
+ width: 100%;
+ height: 150px;
+}
+.previewImg {
+  height: 150px;
+  float:left;
+  border:5px groove gray;
+}
+.elementControlsDiv {
+  position:relative;
+  border:5px groove gray;
+  height: 140px;
+  width: 250px;
+  float: left;
+  padding-top:10px;
+}
+.elementFocusBtn {
+  width: 70%;
+  margin-left:15%;
+}
+.elementMuteBtn {
+  width: 70%;
+  margin-left:15%;
+}
+.elementVolumeBtn {
+  width: 70%;
+  margin-left:15%;
+}
+.elementPlayPauseBtn {
+  width: 70%;
+  margin-left:15%;
+}
+.elementTimeBtn {
+  width: 70%;
+  margin-left:15%;
+}
+`,
+  );
+
+  let styleSheet = document.createElement("style");
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+
+  queryTabs();
+
+  setInterval(async () => {
+    for (const el of document.querySelectorAll(".elementDiv")) {
+      const newdata = await browser.tabs.sendMessage(
+        parseInt(el.getAttribute("tid")),
+        { cmd: "query", id: el.getAttribute("eid") },
+      );
+
+      el.querySelector(".previewImg").setAttribute("src", newdata.poster);
+      el.querySelector(".elementVolumeBtn").setAttribute(
+        "value",
+        newdata.volume * 100,
+      );
+      el.querySelector(".elementVolumeBtn").setAttribute(
+        "title",
+        "Volume: " + newdata.volume * 100,
+      );
+
+      el.querySelector(".elementTimeBtn").setAttribute(
+        "value",
+        newdata.currentTime,
+      );
+      el.querySelector(".elementTimeBtn").setAttribute(
+        "title",
+        "CurrentTime: " + newdata.currentTime,
+      );
+      el.querySelector(".elementMuteBtn").textContent = newdata.muted
+        ? "unmute"
+        : "mute";
+      el.querySelector(".elementPlayPauseBtn").textContent = newdata.playing
+        ? "pause"
+        : "play";
+    }
+  }, 150);
+})();
+
+function isElementInViewport(el) {
+  var rect = el.getBoundingClientRect();
+
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
