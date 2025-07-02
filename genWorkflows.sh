948ac832f94e5b172739ae43240b972afc0c1283
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-07-02 20:12:47 +0200

    feat: add curl to genWorkflows to only generate for flows for addons that are live on AMO

--- genWorkflows.sh
+++ genWorkflows.sh
@@ -1,6 +1,9 @@
 rm -f .github/workflows/*.yml
 for x in *;do 
     if [ -d $x ];then
-        sed "s/PLACEHOLDER/$x/g" workflow-template.yml > .github/workflows/$x.yml
+        CODE=$(curl -sL -I "https://addons.mozilla.org/en-US/firefox/addon/$x" -w "%{http_code}" -o /dev/null)
+        if [ $CODE -eq 200 ];then 
+            sed "s/PLACEHOLDER/$x/g" workflow-template.yml > .github/workflows/$x.yml
+        fi
     fi
 done
