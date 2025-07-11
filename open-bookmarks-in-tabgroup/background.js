4425e5877acc497cfa1f403af8a25a61174abc56
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-07-11 10:42:13 +0200

    fix: only show menu on folders

--- open-bookmarks-in-tabgroup/background.js
+++ open-bookmarks-in-tabgroup/background.js
@@ -1,23 +1,34 @@
 /* global browser */
 
-const manifest = browser.runtime.getManifest();
+let last_visible = true;
+
+browser.menus.onShown.addListener(async (info, tab) => {
+  if (info.bookmarkId) {
+    // prevent needless check
+    const [btNode] = await browser.bookmarks.get(info.bookmarkId); // no idea why this returns an array
+    let curr_visible = typeof btNode.url !== "string"; // only show on folders
+    if (last_visible !== curr_visible) {
+      // prevent needless update+refresh
+      last_visible = curr_visible;
+      await browser.menus.update("obatg", {
+        visible: curr_visible,
+      });
+      browser.menus.refresh();
+    }
+  }
+});
 
 browser.menus.create({
-  title: manifest.name,
+  id: "obatg",
+  title: "Open &Tabgroup",
   contexts: ["bookmark"],
   onclick: async (info, tab) => {
     const [btNode] = await browser.bookmarks.get(info.bookmarkId);
     const createdTabs = [];
-    if (typeof btNode.url === "string") {
+    for (const c of await browser.bookmarks.getChildren(btNode.id)) {
       createdTabs.push(
-        await browser.tabs.create({ url: btNode.url, active: false }),
+        await browser.tabs.create({ url: c.url, active: false }),
       );
-    } else {
-      for (const c of await browser.bookmarks.getChildren(btNode.id)) {
-        createdTabs.push(
-          await browser.tabs.create({ url: c.url, active: false }),
-        );
-      }
     }
     if (createdTabs.length > 0) {
       const groupId = await browser.tabs.group({
