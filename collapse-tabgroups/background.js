0618efd5827076cb141c200839b3bf0e320b05e2
igorlogius <67047467+igorlogius@users.noreply.github.com>
2025-07-18 23:21:50 +0200

    feat: add toolbar button and inverted collapse

--- collapse-tabgroups/background.js
+++ collapse-tabgroups/background.js
@@ -1,59 +1,110 @@
 /* global browser */
 
+browser.browserAction.onClicked.addListener(async (ctab, info) => {
+  (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+    async (tg) => {
+      await browser.tabGroups.update(tg.id, {
+        collapsed: info.button !== 1 && !info.modifiers.includes("Ctrl"),
+      });
+    },
+  );
+});
+
 browser.menus.create({
   title: "Collapse All",
-  contexts: ["tab"],
+  contexts: ["tab", "browser_action"],
   onclick: async (clickdata, ctab) => {
-    (await browser.tabGroups.query({})).forEach(async (tg) => {
-      await browser.tabGroups.update(tg.id, {
-        windowId: ctab.windowId,
-        collapsed: true,
-      });
-    });
+    (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+      async (tg) => {
+        await browser.tabGroups.update(tg.id, {
+          collapsed: true,
+        });
+      },
+    );
   }, // onclick
 });
 
 browser.menus.create({
   title: "Expand All",
-  contexts: ["tab"],
-  onclick: async (clickdata, tab) => {
-    (await browser.tabGroups.query({})).forEach(async (tg) => {
-      await browser.tabGroups.update(tg.id, {
-        windowId: ctab.windowId,
-        collapsed: false,
-      });
-    });
+  contexts: ["tab", "browser_action"],
+  onclick: async (clickdata, ctab) => {
+    (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+      async (tg) => {
+        await browser.tabGroups.update(tg.id, {
+          collapsed: false,
+        });
+      },
+    );
   },
 });
 
 browser.menus.create({
   title: "Collapse Selected",
-  contexts: ["tab"],
+  contexts: ["tab", "browser_action"],
   onclick: async (clickdata, ctab) => {
     if (!ctab.highlighted) {
       await browser.tabGroups.update(ctab.groupId, {
-        windowId: ctab.windowId,
         collapsed: true,
       });
     } else {
-      const processed = new Set();
-      (
-        await browser.tabs.query({
-          highlighted: true,
-          currentWindow: true,
-          pinned: false,
-          hidden: false,
-        })
-      ).forEach(async (t) => {
-        if (t.groupId) {
-          if (!processed.has(t.groupId)) {
-            await browser.tabGroups.update(t.groupId, {
+      const hltgIds = new Set(
+        (
+          await browser.tabs.query({
+            highlighted: true,
+            currentWindow: true,
+            pinned: false,
+            hidden: false,
+          })
+        ).map((t) => t.groupId),
+      );
+
+      (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+        async (tg) => {
+          if (hltgIds.has(tg.id)) {
+            await browser.tabGroups.update(tg.id, {
               collapsed: true,
             });
-            processed.add(t.groupId);
           }
-        }
-      });
+        },
+      );
+    }
+  }, // onclick
+});
+
+browser.menus.create({
+  title: "Collapse NOT Selected",
+  contexts: ["tab", "browser_action"],
+  onclick: async (clickdata, ctab) => {
+    if (!ctab.highlighted) {
+      (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+        async (tg) => {
+          if (tg.id !== ctab.groupId) {
+            await browser.tabGroups.update(tg.id, {
+              collapsed: true,
+            });
+          }
+        },
+      );
+    } else {
+      const hltgIds = new Set(
+        (
+          await browser.tabs.query({
+            highlighted: true,
+            currentWindow: true,
+            pinned: false,
+            hidden: false,
+          })
+        ).map((t) => t.groupId),
+      );
+      (await browser.tabGroups.query({ windowId: ctab.windowId })).forEach(
+        async (tg) => {
+          if (!hltgIds.has(tg.id)) {
+            await browser.tabGroups.update(tg.id, {
+              collapsed: true,
+            });
+          }
+        },
+      );
     }
   }, // onclick
 });
