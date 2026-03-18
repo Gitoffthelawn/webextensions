954e5427594a785172a9c610b7c5969ff6b69941
igorlogius <67047467+igorlogius@users.noreply.github.com>
2026-03-18 16:01:53 +0100

    ref(#29): add try/catch to setItem change setInterval to setTimeout for detect

--- sources/localstorage-editor/content.js
+++ sources/localstorage-editor/content.js
@@ -1,34 +1,47 @@
 /* global browser */
 
-function detect() {
-  const hasSessionStorageData = window.sessionStorage.length > 0;
-  const hasLocalStorageData = window.localStorage.length > 0;
-  // send Message to Background Script to update/show the pageAction icon
-  browser.runtime.sendMessage({ hasSessionStorageData, hasLocalStorageData });
-}
-
-browser.runtime.onMessage.addListener((data, sender) => {
-  localStorage.clear();
-  sessionStorage.clear();
-
-  let el, store, key, value;
-  let i = 0;
-  for (i = 0; i < data.length; i++) {
-    el = data[i];
-    store = el.store;
-    key = el.key;
-    value = el.value;
-    switch (store) {
-      case "Local":
-        localStorage.setItem(key, value);
-        break;
-      case "Session":
-        sessionStorage.setItem(key, value);
-        break;
+(async () => {
+  async function detect() {
+    const hasSessionStorageData = window.sessionStorage.length > 0;
+    const hasLocalStorageData = window.localStorage.length > 0;
+    // send Message to Background Script to update/show the pageAction icon
+    try {
+      await browser.runtime.sendMessage({
+        hasSessionStorageData,
+        hasLocalStorageData,
+      });
+    } catch (e) {
+      console.error(e);
     }
+    setTimeout(detect, 3000);
   }
-});
 
-setInterval(detect, 3000);
+  browser.runtime.onMessage.addListener((data, sender) => {
+    localStorage.clear();
+    sessionStorage.clear();
 
-detect();
+    let el, store, key, value;
+    let i = 0;
+    for (i = 0; i < data.length; i++) {
+      el = data[i];
+      store = el.store;
+      key = el.key;
+      value = el.value;
+      try {
+        switch (store) {
+          case "Local":
+            localStorage.setItem(key, value);
+            break;
+          case "Session":
+            sessionStorage.setItem(key, value);
+            break;
+        }
+      } catch (e) {
+        // setItem can throw an QuotaExceededError
+        window.alert("Failed to Sync Storage Data", e);
+      }
+    }
+  });
+
+  await detect();
+})();
